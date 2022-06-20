c5657cc71850510f8b3c14e39045a4361cb9be67
Adi343 <53225414+Adi343@users.noreply.github.com>
2022-06-20 23:29:23 +0530

    Added react template in frontend folder and added .gitignore file to root folder
    Added create-react-app template and
    removed .gitignore file in backend,frontend folders and added .gitignore file in root folder

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="container">
+      <div class="container--sidebar" />
+
+      <div class="container--main" />
+      <div class="container--sidebar" />
+    </div>
+  );
+}
+
+export default App;
