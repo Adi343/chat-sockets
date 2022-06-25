188bedc767d82b3109dc56a985dc1e0936997273
Adi343 <53225414+Adi343@users.noreply.github.com>
2022-06-25 17:27:34 +0530

    Added ws library and create ChatWindow and ChatInput components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
+import ChatWindow from "./ChatWindow";
 import "./App.css";
 
 function App() {
   return (
     <div class="container">
-      <div class="container--sidebar" />
+      <div className="container--sidebar" />
 
-      <div class="container--main" />
-      <div class="container--sidebar" />
+      <div className="container--main">
+        <ChatWindow />
+      </div>
+      <div className="container--sidebar" />
     </div>
   );
 }
