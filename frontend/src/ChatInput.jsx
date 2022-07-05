3bc105e18cb3cbacce335da0f774548f597f4983
Adi343 <53225414+Adi343@users.noreply.github.com>
2022-07-05 23:00:19 +0530

    Added basic ChatInput component

--- frontend/src/ChatInput.jsx
+++ frontend/src/ChatInput.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ChatInput() {
+  const [chatData, setChatData] = useState("");
+
+  const submitChat = () => {
+    setChatData("");
+  };
   return (
     <div className="chat">
-      <input className="chatBar" />
-      <button>Send</button>
+      <input
+        placeholder={chatData}
+        className="chatBar"
+        onBlur={(e) => {
+          setChatData(e.target.value);
+        }}
+      />
+      <button onClick={submitChat}>Send</button>
     </div>
   );
 }
