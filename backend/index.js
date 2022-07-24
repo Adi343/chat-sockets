b2dc55894f30aa0f1e8c065cfbece09fe19201d9
Adi343 <53225414+Adi343@users.noreply.github.com>
2022-07-24 15:03:46 +0530

    Added dotenv and .env file

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const { WebSocketServer } = require("ws");
 const { createServer } = require("http");
 const express = require("express");
 const fs = require("fs");
+require("dotenv").config();
 
 const app = express();
 const server = createServer(app);
 const wss = new WebSocketServer({ server });
+const port = process.env.PORT;
 
 const data = JSON.parse(fs.readFileSync("./data.json"));
 
@@ -29,6 +31,6 @@ wss.on("connection", function connection(ws) {
     return;
   }, 1000);
 });
-server.listen(8000, () => {
-  console.log("Listening on ws://localhost:8000");
+server.listen(port, () => {
+  console.log(`Listening on ws://localhost:${port}`);
 });
